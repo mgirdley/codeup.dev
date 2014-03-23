6d8a1ee616581828f7e5b72db50797f79e5ca626
Michael Girdley <Michael@codeup.com>
2014-03-22 22:03:58 -0500

    demo parent methods

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -4,6 +4,12 @@ require("filestore.php");
 
 class AddressDataStore extends Filestore {
 
+    function __construct($filename = '') 
+    {
+        parent::__construct(strtolower($filename));
+    }
+
+
     function read_address_book()
     {
       return $this->read_csv();
