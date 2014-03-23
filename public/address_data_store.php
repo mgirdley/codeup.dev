526d8ea64e1a39ec0d3ef49ffe7bf8052b78a8f1
Michael Girdley <Michael@codeup.com>
2014-03-22 22:00:19 -0500

    removed comment blocks

--- public/address_data_store.php
+++ public/address_data_store.php
@@ -6,18 +6,6 @@ class AddressDataStore extends Filestore {
 
     function read_address_book()
     {
-      // $temp_address_book = [];
-      // $row = 1;
-      // if (($handle = fopen($this->filename, "r")) !== FALSE) {
-      //    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-      //        $num = count($data);
-      //        $temp_entry = [$data[0],$data[1],$data[2],$data[3],$data[4]];
-      //        array_push($temp_address_book,$temp_entry);
-      //    }
-      //   fclose($handle);
-      // }
-      // echo "temp addy boook:";
-      // var_dump($temp_address_book);
       return $this->read_csv();
     }
     
@@ -25,14 +13,6 @@ class AddressDataStore extends Filestore {
     {
 
       $this->write_csv($items);
-    //   $handle = fopen($this->filename, 'w');
-    //   // var_dump($items);
-    //   foreach ($items as $fields) {
-    //     fputcsv($handle, $fields);
-    //     // var_dump($fields);
-    //   }
-    // fclose($handle);
-        // Code to write $addresses_array to file $this->filename
     }
 
 } 
